c6b83e91b6fc9a09f24ead7c17d1c308b2e1bc47
chronosgit <warfacemeister@mail.ru>
2024-01-20 16:18:17 +0600

    Keyboard users can now click colors

--- src/js/_colorButton.js
+++ src/js/_colorButton.js
@@ -3,21 +3,22 @@ import { gameSession } from "./_playButton.js";
 const colorButtons = document.querySelectorAll(".color");
 let colorEventListeners = [];
 
-const clickColor = (e) => {
+const clickColor = (color) => {
     if(!gameSession) return;
-
-    const color = e.target;
+    
     const colorType = color.getAttribute("id").slice(6);
 
     gameSession.chooseColor(colorType);
 }
 
 colorButtons.forEach(colorBtn => {
-    const colorEventListener = colorBtn.addEventListener("click", clickColor);
+    const colorEventListener = colorBtn.addEventListener("click", function() {
+        clickColor(this) 
+    });
     colorEventListeners.push(colorEventListener);
     colorBtn.onkeydown = (key) => {
-        if(key.code === 13) {
-            clickColor();
+        if(key.keyCode === 13) {
+            clickColor(colorBtn);
         }
     }
 });
